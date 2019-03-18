019ac8d3c4c8c9ebb631b0f739ccdcfed554d660
aszoke2 <zoke2556@gmail.com>
2019-03-17 21:14:29 -0400

    drag n drop works

--- js/main.js
+++ js/main.js
@@ -5,21 +5,82 @@ function allowDrop(ev) {
 }
 
 function teamIsDraggingEvent(ev) {
-	ev.dataTransfer.setData("text", ev.target.id);
+	var tid = ev.target.id;
+	ev.dataTransfer.setData("Text", tid);
+	var team = findTeamById(parseInt(tid.substring(5)))[0];
+	
+	var dragFromRd = $(ev.target).parents().eq(2).attr('class').substring(12);
+	onTeamDrag(team, parseInt(dragFromRd));
 }
 
 function drop(ev) {
 	ev.preventDefault();
-	var data = ev.dataTransfer.getData("text");
-	console.log(data);
-	ev.target.appendChild(document.getElementById(data));
-}
-
-function assignTeamToMatchup(team, matchup) {
+	var data = ev.dataTransfer.getData("Text");
+	var team = findTeamById(parseInt(data.substring(5)))[0]; // chop the team id out of the div name
+	var confirmedTarget;
+	var evictedTeam = "";
 	
+	// handle the drop depending on what the team was dropped on
+	if ($(ev.target).hasClass('team')) { // team dropped on an empty slot
+		confirmedTarget = ev.target;
+	} else if ($(ev.target).hasClass('teamObj')) { // team dropped on an occupied slot
+		confirmedTarget = ev.target.parentNode;
+		var evictedTeam = findTeamById(parseInt($(ev.target).attr('class').substring(5)))[0];
+	} else if ($(ev.target).hasClass('fa-trophy')) { // team picked as tourney winner
+		confirmedTarget = ev.target;
+	} else {
+			console.log('Invalid drop target ' + $(ev.target));
+			droppedOnRd = "0";
+		}
+	
+	// find the chosen round, check for first four rounds first
+	var droppedOnRd = $(confirmedTarget).parents().eq(1).attr('class').substring(12);
+	if (droppedOnRd === "") { // user dropped on final 4 or champ round
+		droppedOnRd = $(confirmedTarget).parent().attr('class').substring(27);
+	}
+	if (droppedOnRd === "") { // check if user dropped on trophy
+		if ($(confirmedTarget).hasClass('fa-trophy')) {
+			droppedOnRd = "7";
+		} 
+	}
+	
+	if (evictedTeam != "") {
+		clearTeamForward(team, droppedOnRd);
+	}
+	
+	confirmedTarget.appendChild(document.getElementById(data));
+	onTeamDropped(team, parseInt(droppedOnRd));
 }
 
-function makeBackwardSlotsReceptive(input) {
+function clearTeamForward(team, fromRd) {
+	var curSlot;
+	var slots = findPossibleForwardSlots(team);
+	
+	for (var i = fromRd; i <= 7; i++) {
+		var curCoord = slots[i - 2];
+		if (i <= 4) {
+			curSlot = '#' + team.team_region + ' .round.round-' + i + ' ul.matchup:nth-of-type(' + curCoord[0] + ') li.team.team-' + topOrBottom(curCoord[1]);
+		} else if (i === 5) {
+			curSlot = '.champion .semis-' + curCoord[0] + ' li.team.team-' + topOrBottom(curCoord[1]);
+		} else if (i === 6) {
+			curSlot = '.final ul.championship li.team.team-' + topOrBottom(curCoord);
+		} else if (i === 7) {
+			curSlot = '.fa.fa-trophy';
+		}
+		
+		if ($(curSlot).children('#team_' + team.team_id) != null) {
+			$(curSlot)
+				.empty()
+				.removeClass('set receptive')
+				.addClass('unset');
+		} else {
+			break;
+		}
+		
+	}
+}
+
+function onGenerate() {
 	
 }
 
@@ -64,7 +125,7 @@ function replacePlayInWithPseudoTeams() {
 }
 
 function teamToDiv(team) {
-	return ('<div id="team_' + team.team_id + '" draggable="true" ondrag="teamIsDraggingEvent(event)">' + team.team_name + '<span class="seed">' + team.team_seed + '</span></div>');
+	return ('<div class="teamObj" id="team_' + team.team_id + '" draggable="true" ondragstart="teamIsDraggingEvent(event)">' + team.team_name + '<span class="seed">' + team.team_seed + '</span></div>');
 }
 
 function topOrBottom(sel) {
@@ -121,14 +182,14 @@ function convertSeedToCoordinate(seed) {
 function findPossibleForwardSlots(team) {
 	var r1Coord = convertSeedToCoordinate(team.team_seed);
 	var curRdNth = r1Coord[0];
-	var tob = r1Coord[1];
+	var tob;
 	var res = [];
 	
 	// handle the regional rounds first
 	for (var i = 2; i <= 4; i++) {
-		tob = (curRdNth - 1) % 2;
-		curRdNth = Math.ceil(curRdNth/2);
-		res.push([curRdNth, tob]);
+		var nextRdPos = findNextRdSlot(curRdNth);
+		curRdNth = nextRdPos[0];
+		res.push(nextRdPos);
 	}
 	
 	// handle the final 4 and champ rounds separately
@@ -163,30 +224,80 @@ function findTeamById(id) {
 }
 
 function onTeamClicked(team, roundNo) {
-	alert(team.team_id + " " + roundNo);
-}
-
-function onTeamMouseUp(team, roundNo) {
-	console.log(team.team_id + " mouseup");
-}
-
-function makeForwardSlotsReceptive(team, roundNo) {
-	var slots = findPossibleForwardSlots(team);
-	var curRd = roundNo + 1;
 	
-	for (var i = roundNo - 1; i <= 4; i++) {
-		var curSlot = slots[i];
-		var newClass = 'team team-' + topOrBottom(curSlot[1]) + ' receptive';
-		if (i <= 2) {
-			var oldClass = '#' + team.team_region + ' .round.round-' + (i + 2) + ' ul.matchup:nth-of-type(' + curSlot[0] + ') li.team.team-' + topOrBottom(curSlot[1]);
-		} else if (i === 3) {
-			var oldClass = '.champion .semis-' + curSlot[0] + ' li.team.team-' + topOrBottom(curSlot[1]);
-		} else if (i === 4) {
-			var oldClass = '.final .championship li.team.team-' + topOrBottom(curSlot);
+}
+
+function findNextRdSlot(curRdNth) {
+	var tob = (curRdNth - 1) % 2;
+	var nextRdNth = Math.ceil(curRdNth/2);
+	return [nextRdNth, tob];
+}
+
+function onTeamDropped(team, toRd) {
+	var rd2Onward = findPossibleForwardSlots(team);
+	var slots = [convertSeedToCoordinate(team.team_seed)].concat(rd2Onward);
+	
+	for (var i = 1; i <= 7; i++) {
+		var curSlot = slots[i - 1];
+		console.log(curSlot);
+		var oldClass, newClass;
+		if (i <= 4) {
+			oldClass = '#' + team.team_region + ' .round.round-' + i + ' ul.matchup:nth-of-type(' + curSlot[0] + ') li.team.team-' + topOrBottom(curSlot[1]);
+		} else if (i === 5) {
+			oldClass = '.champion .semis-' + curSlot[0] + ' li.team.team-' + topOrBottom(curSlot[1]);
+		} else if (i === 6) {
+			oldClass = '.final ul.championship li.team.team-' + topOrBottom(curSlot);
+		} else if (i === 7) {
+			oldClass = '.fa.fa-trophy';
 		}
-		$(oldClass).attr('class', newClass);
+		
+		$(oldClass)
+			.removeClass('receptive')
+			.removeAttr('ondrop, ondragover');
+		if (i <= toRd) {
+			$(oldClass)
+				.empty()
+				.removeClass('unset')
+				.addClass('set')
+				.attr('class', newClass)
+				.append(teamToDiv(team));
+		}
 	}
-	//$('i.fa.fa-trophy').attr('class', 'i.fa.fa-trophy.receptive');
+}
+
+function onTeamDrag(team, fromRd) {
+	var slots = findPossibleForwardSlots(team);
+	var curRd = fromRd + 1;
+	var oldClass;
+	
+	for (var i = fromRd - 1; i <= 4; i++) {
+		var curSlot = slots[i];
+		if (i <= 2) {
+			oldClass = '#' + team.team_region + ' .round.round-' + (i + 2) + ' ul.matchup:nth-of-type(' + curSlot[0] + ') li.team.team-' + topOrBottom(curSlot[1]);
+		} else if (i === 3) {
+			oldClass = '.champion .semis-' + curSlot[0] + ' li.team.team-' + topOrBottom(curSlot[1]);
+			$('#' + team.team_region + ' .mobile-f4-pick')
+				.attr({
+					ondrop: 'drop(event)',
+					ondragover: 'allowDrop(event)'
+				})
+				.addClass('receptive');
+		} else if (i === 4) {
+			oldClass = '.final .championship li.team.team-' + topOrBottom(curSlot);
+		}
+		$(oldClass)
+			.attr({
+				ondrop: 'drop(event)',
+				ondragover: 'allowDrop(event)'
+			})
+			.addClass('receptive');
+	}
+	$('.fa.fa-trophy')
+		.attr({
+			ondrop: 'drop(event)',
+			ondragover: 'allowDrop(event)'
+		})
+		.addClass('receptive');
 }
 
 function populateBracket() {
@@ -194,14 +305,9 @@ function populateBracket() {
 		var coord = convertSeedToCoordinate(team.team_seed);
 		var slot = '#' + team.team_region + ' .round-1 ul.matchup:nth-of-type(' + coord[0] + ') .team-' + topOrBottom(coord[1]);
 		$(slot).html(teamToDiv(team));
+		var test = $(slot).parents().eq(1).attr('class');
 		var teamDiv = slot + " #team_" + team.team_id;
 		$(teamDiv)
-			.mousedown(function() {
-				makeForwardSlotsReceptive(team, 1);
-			})
-			.mouseup(function() {
-				onTeamMouseUp(team, 1);
-			})
 			.click(function() {
 				onTeamClicked(team, 1);
 			});
@@ -211,13 +317,16 @@ function populateBracket() {
 $(document).ready(function() {
 	
 	$(function() {
-		Papa.parse("https://projects.fivethirtyeight.com/march-madness-api/2019/fivethirtyeight_ncaa_forecasts.csv", {
+		Papa.parse("https://projects.fivethirtyeight.com/march-madness-api/2018/fivethirtyeight_ncaa_forecasts.csv", {
 			delimiter: ",",
 			download: true,
 			header: true,
 			dynamicTyping: true,
 			skipEmptyLines: "greedy",
 			complete: function(results) {
+				results.data = $.grep(results.data, function(line) {
+					return line.gender === 'mens' && line.forecast_date === '2018-03-31';
+				});
 				console.log(results);
 				teamData = results.data;
 				replacePlayInWithPseudoTeams();
